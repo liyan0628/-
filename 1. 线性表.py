cee4916f0f020f58381f489e8e1b7a787296038f
liyan <34911370+liyan0628@users.noreply.github.com>
2021-08-12 20:14:47 +0800

    Update and rename 线性表.py to 1. 线性表.py

---
+++ 1. 线性表.py
@@ -0,0 +1,11 @@
+class singleNode:
+  def __init__(self,item):
+    self.item = item
+    self.next = None
+class singleLink:
+  def __init__(self):
+    self.head = None
+  def isEmpty(self):
+    return self.head == None
+  
+    
