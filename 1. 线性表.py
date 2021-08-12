aed8a96baed43c1d72a3317e9278a438f6c6a86d
liyan <34911370+liyan0628@users.noreply.github.com>
2021-08-12 20:24:08 +0800

    Update 1. 线性表.py

--- 1. 线性表.py
+++ 1. 线性表.py
@@ -7,5 +7,21 @@ class singleLink:
     self.head = None
   def isEmpty(self):
     return self.head == None
+  def travel(self):
+    if not self.head:
+      return
+    cur = self.head
+    while cur:
+      print(cur.item,end = ' ')
+      cur = cur.next
+  def length(self):
+    result = 0
+    if not self.head:
+      return result
+    cur = self.head
+    while cur:
+      result += 1
+      cur = cur.next
+    return result
   
     
